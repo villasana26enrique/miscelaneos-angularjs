bab9239ed3fe4d5644c3903d18e5538974e02172
villasana26enrique <enrique.villasana26@gmail.com>
2020-05-23 13:37:25 -0400

    Aprendiendo como los como el css y el Scope. Aplicando CSS a un componente especifico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    CssComponent
   ],
   imports: [
     BrowserModule,
