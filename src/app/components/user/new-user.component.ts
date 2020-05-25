559e758c16fbbd65a207ac38a567059c9663e068
villasana26enrique <enrique.villasana26@gmail.com>
2020-05-24 22:20:59 -0400

    Obteniendo el valor que se pasa en la ruta padre, desde una ruta hija

--- src/app/components/user/new-user.component.ts
+++ src/app/components/user/new-user.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,7 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute ) {
+    /* Se utiliza "parent" para obtener el valor que se pasa a la ruta Padre
+    que en este caso es la ruta /user */
+    this.route.parent.params.subscribe( params => {
+      console.log('Ruta Hija (New User)');
+      console.log(params);
+    });
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,12 +1,17 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html'
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute ) {
+    this.route.params.subscribe( params => {
+      console.log('Ruta Padre');
+      console.log(params);
+    });
+  }
 
   ngOnInit(): void {
   }
